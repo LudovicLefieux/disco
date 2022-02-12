e739421c12c62e2ec0a2aaf2eac1bd3572f09904
LudovicLefieux <lefieux.ludovic@protonmail.com>
2022-02-12 16:15:37 +0100

    Add Readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# disco
+# Disco
+
+This project is a reproduction of the [Disco extension's website](https://www.getdisco.com/). I am making it for fun and to practice my skills.
--- single.php
+++ single.php
@@ -1,68 +1,83 @@
 <?php get_header(); ?>
+
+<main class="main">
+  <div class="breadcrumbs section">
+    <div class="breadcrumbs__container container">
+      <ul class="breadcrumbs__list">
+        <li class="breadcrumbs__list__item"><a href="#"><i class="fas fa-home"></i></a></li>
+        <li class="breadcrumbs__list__item"><a href="#">Blog</a></li>
+        <li class="breadcrumbs__list__item"><a href="#">All posts</a></li>
+        <li class="breadcrumbs__list__item"><a href="#">Kena: Bridge of Spirits</a></li>
+      </ul>
+    </div>
+  </div>
   <?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
     
-    <article class="post">
-      <?php the_post_thumbnail(); ?>
-
-      <h1><?php the_title(); ?></h1>
-
-      <div class="post__meta">
-        <?php echo get_avatar( get_the_author_meta( 'ID' ), 40 ); ?>
-        <p>
-          Publié le <?php the_date(); ?>
-          par <?php the_author(); ?>
-          Dans la catégorie <?php the_category(); ?>
-          Avec les étiquettes <?php the_tags(); ?>
-        </p>
-      </div>
-
-      <div class="post__content">
-        <?php the_content(); ?>
-        <p>
-            <strong>Avis :</strong> 
-            <?php echo get_post_meta( get_the_ID(), 'avis', true ); ?>
-        </p>
-
-        <p>
-            <strong>Note :</strong>
-            <?php echo get_post_meta( get_the_ID(), 'note', true ); ?> / 10
-        </p>
-
-        <div class="plus-moins">
-            <div class="plus">
-                <?php echo get_post_meta( get_the_ID(), 'plus', true ); ?>
-            </div>
-            <div class="plus">
-                <?php echo get_post_meta( get_the_ID(), 'moins', true ); ?>
-            </div>
+    <article class="post section">
+      <div class="post__container container">
+        <div class="post__img-container">
+          <?php the_post_thumbnail(); ?>
         </div>
-        <?php if( has_category( 'jeux-video' ) ): ?>
 
-            <div class="review">
-                <div class="review__score">Note : <?php the_field( 'note' ); ?> / 10</div>
+        <div class="post__layout">
+          <div class="post__meta">
+            <div class="post__meta__tags">
+              <?php the_tags('', '', ''); ?>
+            </div>
+            <p class="post__meta__date"><?php the_date(); ?></p>
+          </div>
 
-                <div class="review__cols">
-                    <div class="review__pros">Les plus : <?php the_field( 'les_plus' ); ?></div>
-                    <div class="review__cons">Les moins : <?php the_field( 'les_moins' ); ?></div>
+          <div class="post__content">
+            <h1><?php the_title(); ?></h1>
+            <?php the_content(); ?>
+            <!--
+            <p>
+                <strong>Avis :</strong> 
+                <?php echo get_post_meta( get_the_ID(), 'avis', true ); ?>
+            </p>
+
+            <p>
+                <strong>Note :</strong>
+                <?php echo get_post_meta( get_the_ID(), 'note', true ); ?> / 10
+            </p>
+
+            <div class="plus-moins">
+                <div class="plus">
+                    <?php echo get_post_meta( get_the_ID(), 'plus', true ); ?>
                 </div>
+                <div class="plus">
+                    <?php echo get_post_meta( get_the_ID(), 'moins', true ); ?>
+                </div>
+            </div>
+            <?php if( has_category( 'jeux-video' ) ): ?>
 
-                <div class="review__date">Sortie le <?php the_field( 'date_de_sortie' ); ?></div>
+                <div class="review">
+                    <div class="review__score">Note : <?php the_field( 'note' ); ?> / 10</div>
 
-                
-                    <div class="review__picture">
-                        <?php 
-                          $image_id = get_field( 'pochette' ); // On récupère cette fois l'ID
-                          if( $image_id ) {	
-                            echo wp_get_attachment_image( $image_id, 'full' );
-                          }
-                        ?>
+                    <div class="review__cols">
+                        <div class="review__pros">Les plus : <?php the_field( 'les_plus' ); ?></div>
+                        <div class="review__cons">Les moins : <?php the_field( 'les_moins' ); ?></div>
                     </div>
-              </div>
 
-        <?php endif; ?>
+                    <div class="review__date">Sortie le <?php the_field( 'date_de_sortie' ); ?></div>
+
+                    
+                        <div class="review__picture">
+                            <?php 
+                              $image_id = get_field( 'pochette' ); // On récupère cette fois l'ID
+                              if( $image_id ) {	
+                                echo wp_get_attachment_image( $image_id, 'full' );
+                              }
+                            ?>
+                        </div>
+                  </div>
+
+            <?php endif; ?>
+          </div>
+        </div>
+
+        <?php comments_template(); // Par ici les commentaires ?>-->
       </div>
-
-      <?php comments_template(); // Par ici les commentaires ?>
     </article>
 
   <?php endwhile; endif; ?>
@@ -75,4 +90,6 @@
           <?php next_post_link( 'Article Suivant<br>%link' ); ?> 
       </div>
   </div>
+</main>
+
 <?php get_footer(); ?>
